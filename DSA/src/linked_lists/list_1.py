c857145d0c72f5b2ac3e9a1f3315f008f96005fc
tomfyfe <tomfyfe85@gmail.com>
2025-06-07 13:42:15 +0100

    pop method seems to work

--- DSA/src/linked_lists/list_1.py
+++ DSA/src/linked_lists/list_1.py
@@ -22,6 +22,26 @@ class Linked_List:
 
         return True
 
+    def pop(self):
+        current_node = self.head.next
+        next_node = current_node.next
+        if current_node is None:
+            return None
+
+        while current_node:
+            print("current", current_node.data)
+            print("next", next_node)
+            if next_node.next is None:
+                print("loop")
+                current_node.next = None
+                self.tail = current_node
+                break
+            current_node = next_node
+            next_node = current_node.next
+            print("end")
+
+        return None
+
     def print_list(self):
         temp = self.head
         while temp is not None:
@@ -29,6 +49,11 @@ class Linked_List:
             temp = temp.next
 
 
-myList = Linked_List(4)
-myList.append(5)
+myList = Linked_List(0)
+myList.append(1)
+myList.append(2)
+myList.append(3)
+myList.append(4)
+myList.pop()
+
 myList.print_list()
